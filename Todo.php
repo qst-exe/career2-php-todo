128496cce5f223baf98b359175c455c53122e548
Task Nagashige <task.19.xx@gmail.com>
2020-10-07 12:26:50 +0900

    アップロードした画像を表示できるようにした

--- Todo.php
+++ Todo.php
@@ -32,7 +32,7 @@ class Todo
      */
     public function getList()
     {
-        $stmt = $this->dbh->query("SELECT * FROM `todo` WHERE `deleted_at` IS NULL ORDER BY `due_date` ASC");
+        $stmt = $this->dbh->query("SELECT id, title, due_date, image, created_at, deleted_at FROM `todo` WHERE `deleted_at` IS NULL ORDER BY `due_date` ASC");
         return array_map(function ($todo) {
             $todo["status"] = intval($todo["status"]);
             $todo["status_for_display"] = self::STATUS[$todo["status"]];
